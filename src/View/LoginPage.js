63ca047550238bae197ec669e42910b3816bd8e8
Tóvári Tamás <kuablaikan@hotmail.com>
2021-06-09 12:04:57 +0200

    password input field minor fix

--- src/View/LoginPage.js
+++ src/View/LoginPage.js
@@ -16,7 +16,7 @@ export function InitPage(){
     const html = `<label for="username">Felhasználó név:</label><br>
     <input type="text" id="username" name="email" value=""><br>
     <label for="password">Jelszó:</label><br>
-    <input type="text" id="password" name="password" value=""><br><br>
+    <input type="password" id="password" name="password" value=""><br><br>
     <input id="login" type="button" value="Bejelentkezés">`;
 
     loginPage.Show("Login ","form", html);
