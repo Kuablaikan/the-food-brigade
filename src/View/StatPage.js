909a4d035a7479fe2b6827c05c8b6e717efc8cc9
vparon <vpa.2000.szolgalat@gmail.com>
2021-06-17 12:03:20 +0200

    mar egesz jó, a diagram csak kalibráni kell, de már a jó adatokkal megy

--- src/View/StatPage.js
+++ src/View/StatPage.js
@@ -1,6 +1,7 @@
 import { Content } from "./Content.js";
 import {OrderService} from "./../Service/OrderService.js";
 import {OrderItemService} from "./../Service/OrderItemService.js";
+import {CheeseService} from "../Service/CheeseService.js";
 
 export function InitPage(){
     const MystatPage = new Content();
@@ -12,6 +13,21 @@ export function InitPage(){
     let Dates = [];
     let OrderIDs = [];
     let OrderItemsbyID = [];
+    let CheeseData = [];
+    let CheeseIDs = [];
+
+
+    CheeseData = CheeseService.getAll();
+
+    //console.log(CheeseData);
+
+    for(let i = 0; i<CheeseData.length; i++)
+    {
+        CheeseIDs[i] = CheeseIDs.push(CheeseData[i].id);;
+
+        //console.log(CheeseIDs[i]);
+    }
+    //console.log(CheeseIDs);
     
     AllOrders = OrderService.getAll();
     
@@ -21,12 +37,30 @@ export function InitPage(){
             OrderIDs[i] = AllOrders[i].id;
             
             let id = OrderIDs[i];
-            OrderItemsbyID[i] = OrderItemService.getByOrderId(id);
+            OrderItemsbyID[i] = OrderItemsbyID.push(OrderItemService.getByOrderId(id));
             
             let teszt = OrderItemsbyID[i];
-            console.log(teszt);
+            //console.log(teszt);
         }
-    
+
+    let AllOrderItems = OrderItemService.getAll();
+    let sumQuantity = new Map();
+    for(let i = 0; i < AllOrderItems.length; i++)
+    {
+        const key = AllOrderItems[i].cheeseId;
+        if (!sumQuantity.has(key))
+            sumQuantity.set(key, 0);
+        sumQuantity.set(key, sumQuantity.get(key) + 1);
+    }
+
+    for (const pair of sumQuantity.entries()) {
+        //console.log(pair);
+    }
+   // console.log(sumQuantity.get(3));
+
+
+
+
     var canvas = document.getElementById('test');
     var ctx = canvas.getContext('2d');
 
@@ -40,18 +74,21 @@ export function InitPage(){
     ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
     ctx.stroke();
     */
-    let testData = [ 20, 40, 70, 110, 30, 40, 31, 80];
+    //let testData = [ 20, 40, 70, 110, 30, 40, 31, 80];
     let width = 50;
     let currX = 50;
     let base = 200;
     
     ctx.fillStyle = 'green';
-    for(let i = 0; i<testData.length; i++)
+    for(let i = 0; i<sumQuantity.size; i++)
         {
-            let h = testData[i];
-            ctx.fillRect(currX, canvas.height - h, width, h);
-            currX += width + 10;
-            
+            for(let j = 0; j < AllOrderItems.length; j++)
+            {
+                let h = sumQuantity.get(AllOrderItems[j].cheeseId);
+                console.log(h);
+                ctx.fillRect(currX, canvas.height - h, width, h);
+                currX += width + 10;
+            }
         }
 
     return MystatPage;
